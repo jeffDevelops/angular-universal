de9dff36c28acbf7c8d5777632ed6beef4307f0c
jeffDevelops <jefferydreynolds@gmail.com>
2017-08-14 16:07:32 -0600

    Creates git repository for code review

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(meta: Meta, title: Title) {
+  	meta.addTags([
+	  	{name: 'author', content: 'Jeff Reynolds'},
+	  	{name: 'keywords', content: 'angular seo, angular 4 universal, etc.'},
+	  	{name: 'description', content: 'This is my Angular SEO=based App, enjoy it!'}
+  ]);
+  }
+
 }
